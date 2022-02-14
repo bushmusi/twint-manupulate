b704503b30c537c0a4573f768b4e2ea7c4456694
bushmusi <bushera.mestofa@gmail.com>
2022-02-14 09:01:31 +0300

    Add server.js file

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database success!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+  };
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to twint application." });
+});
+
+const router = (global.router = (express.Router()));
+app.use('/api/arsenal', require('./app/routes/arsenal.routes'));
+app.use('/api/analyse', require('./app/routes/analyse.routes'));
+app.use(router);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
