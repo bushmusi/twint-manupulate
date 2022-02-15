e84ceeb2d290dde03a4b84099c825bfb7c33fc18
bushmusi <bushera.mestofa@gmail.com>
2022-02-15 14:39:43 +0300

    Update new route

--- app/routes/analyse.routes.js
+++ app/routes/analyse.routes.js
@@ -1,18 +1,12 @@
+const express = require('express');
+const { model } = require('mongoose');
+const modelCont = require('../controllers/analyse.controller')
 
-const analyse = require("../controllers/analyse.controller.js");
-var router = global.router
+const router = express.Router();
 
-router.get('/map',analyse.mapTweets);
+router.get('/multi-param-search',modelCont.dbGetTable)
+router.get('/map-reduce',modelCont.mapReduceTable)
+router.get('/twint-filter',modelCont.twintFilter)
+router.get('/db-meta-data',modelCont.dbMetadata)
 
 module.exports = router
-
-module.exports = app => {
-    const arsenal = require("../controllers/analyse.controller");
-  
-    var router = require("express").Router();
-  
-    // Retrieve all arsenal
-    router.get("/", arsenal.findAll);
-  
-    app.use('/api/arsenal', router);
-  };
